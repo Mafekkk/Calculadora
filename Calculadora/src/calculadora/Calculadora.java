19b01c264c7267a7e754e1d1b3f1c7177f4b0712
Estudiantes <Estudiantes@labing50407.udistrital.edu.co>
2017-08-25 17:03:36 -0500

    crear las clases

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,9 +1,31 @@
 package calculadora;
 
-/**
- *
- * @author Estudiantes
- */
+
 public class Calculadora {
+    float operando1=1;
+    float operando2=0;
+    float resultado;
     
+    public float sumar (){
+        resultado=operando1 + operando2;
+        return resultado;
+    }
+    public float restar(){
+        resultado=operando1-operando2;
+        return resultado;
+    }
+    public float multiplicar (){
+        resultado=operando1*operando2;
+        return resultado;
+    }
+    public float dividir (){
+        if(operando2==0){
+            return 0;
+        }
+        else{
+            resultado=operando1/operando2;
+            return resultado;
+        }
+        
+    }
 }
---
+++ Calculadora/src/calculadora/Principal.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+
+public class Principal {
+
+
+    public static void main(String[] args) {
+        Calculadora miCalc= new Calculadora();
+        System.out.println(miCalc.dividir());
+    }
+    
+}
